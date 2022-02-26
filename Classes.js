961423dbb8c028ec58f54b5f404b8acda44afbb7
Cullen REDDY <cull.redd@gmail.com>
2022-02-26 12:13:43 -0800

    more classes

--- Classes.js
+++ Classes.js
@@ -10,7 +10,7 @@ class Dog {
   get behavior() {
     return this._behavior;
   }   
-
+0
   incrementBehavior() {
     this._behavior ++;
   }
@@ -22,3 +22,27 @@ console.log(halley.behavior); // Print behavior value to console
 halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
+
+class Cat {
+  constructor(name, usesLitter) {
+    this._name = name;
+    this._usesLitter = usesLitter;
+    this._behavior = 0;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  get usesLitter() {
+    return this._usesLitter;
+  }
+ 
+  get behavior() {
+    return this._behavior;
+  }  
+ 
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
